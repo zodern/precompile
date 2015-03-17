1551add2d807f9b36d6a4ff2b12a723349e9b86f
zodern <zodern@users.noreply.github.com>
2015-03-17 18:11:02 -0500

    fixed paths in upload.js

--- upload.js
+++ upload.js
@@ -4,11 +4,11 @@ var storage;
 
 // Or from elsewhere: 
 storage = gcloud.storage({
-  keyFilename: '../../gs.json',
+  keyFilename: '../gs.json',
   projectId: 'nodeos-speed'
 });
 
 var bucket = storage.bucket('pre-compiled');
 
-fs.createReadStream('./out.zip').pipe(bucket.file('out.zip').createWriteStream());
+fs.createReadStream('./upload/out.zip').pipe(bucket.file('out.zip').createWriteStream());
 
